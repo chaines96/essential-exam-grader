80eb81f0f83460dd0fb4657e00503ab0d79a50a7
chaines96 <haineschris777@gmail.com>
2026-01-03 22:53:35 -0700

    Added support for selections in multiple selction files for each uqiz. Also support for using previously calculated scores.

--- quickmark-report.py
+++ quickmark-report.py
@@ -1,95 +1,130 @@
 import csv
 
-#File variables
-entrants_file = open("entrants.csv")
-entrants  = csv.reader(entrants_file, delimiter=',')
-try:
-    student_answers_file = open("student_answers.csv")
-    student_answers  = csv.reader(student_answers_file, delimiter=',')
-except Exception as e:
-    print(e)
-answer_key_file = open("answer_key.csv")
-answer_key  = csv.reader(answer_key_file, delimiter=',')
+selections = list() 
 
-#If the picks were not a single CSV file, we will make a list out of new ones.
-picks_list = list()
-picks_list_num = []
-quiz_index = 1
-while(quiz_index < 30):
+#File variables
+TOTAL_QUIZZES = 25
+entrants_file = open("entrants.csv", encoding="utf8")
+entrants = csv.reader(entrants_file, delimiter=',')
+try:
+    selections_file = open("selections.csv")
+    selections  = csv.reader(selections_file, delimiter=',')
+except Exception as e:
+    pass
+answers_file = open("answers.csv")
+answers = csv.reader(answers_file, delimiter=',')
+
+#If the selections were not a single CSV file, we will make a list out of new ones.
+selections_list = list()
+selections_list_num = []
+quiz_index = 0
+while(quiz_index < TOTAL_QUIZZES):
+    quiz_index = quiz_index + 1
     try:
-        picks_quizfile = open("Quiz_" + str(quiz_index) + "_Report.csv")
-        picks_list.append(csv.reader(picks_quizfile, delimiter=','))
-        picks_list_num.append(quiz_index)
+        selections_quizfile = open("Quiz" + str(quiz_index) + "_Selections.csv", encoding="utf8")
+        selections_list.append(csv.reader(selections_quizfile, delimiter=','))
+        selections_list_num.append(quiz_index)
     except Exception as e:
         pass
-    quiz_index = quiz_index + 1
 
 scoreboard = dict() #This is a dictionary whose keys are tuples (email,name) and whose values are lists
 
 #Populate the "score board"
 for entrant in entrants:
-    blank_list = [0]*26
-    scoreboard.update({(entrant[0],entrant[1]) : blank_list})
+    blank_list = [0]*(TOTAL_QUIZZES+1)
+    scoreboard.update({(entrant[0].lower().replace(" ",""),entrant[1].lower().replace(" ","")) : blank_list})
+
+#Read the scoreboard from a previous file
+try:
+    previous_scores_file = open("prev.csv")
+    previous_scores = csv.reader(previous_scores_file, delimiter=",")
+except Exception as e:
+    print(e)
+try:
+    for score in previous_scores:
+        new_list = [0]*(TOTAL_QUIZZES+1) #Minus two lines to account for the name and email.
+        for h in range(0,(len(score) - 3)): #We substract one from the length of score here because the total can be zero at this stage.
+            try:
+                new_list[h] = int(score[h+2])
+            except Exception as e:
+                print("Problem occured importing the score for " + score[1])
+                print(e)
+        scoreboard.update({(score[0].lower().replace(" ",""),score[1].lower().replace(" ","")) : new_list})
+except Exception as e:
+    print(e)
 
 #Variables
 QUIZ = 0
 comparison = list()
-amount_of_quizzes = 25 #How many games played this quiz
+amount_of_questions = 25 #How many questions in this quiz.
 
-#Evaluate each pick, of every quiz.
-for pick in picks:
-    if pick[-1] != QUIZ: #only runs if quiz is different
+#Evaluate each pick, of every week.
+for pick in selections:
+    if pick[-1] != QUIZ: #only runs if week is different
         QUIZ = pick[-1]
         try:
-            int(QUIZ) #If the last column is a number, by convention, we consider that the quiz number and we use it instead of the quiz index.
+            int(QUIZ) #If the last column is a number, by convention, we consider that the week number and we use it instead of the week index.
         except Exception as e:
             print(e)
             continue
-        for answer in answer_key:
-            #This finds the quiz of the given pick
+        for answer in answers:
+            #This finds the week of the given pick
             if answer[0] == QUIZ:
                 comparison = answer[:]
-                amount_of_quizzes = len(comparison) - 4
+                amount_of_questions = len(comparison) - 1
     #The comparison array now contains the correct values.
-    quiz_score = 0
-    if scoreboard[(pick[0],pick[1])][int(QUIZ)-1] == 0: #Newer entries come first in the list, so a duplicate entry is skipped.
-        for i in range(2,amount_of_quizzes):
-            if pick[i] == comparison[i-1]:
-                quiz_score = quiz_score + 1
+    weekly_score = 0
+    try:
+        if scoreboard[(pick[0].lower().replace(" ",""),pick[1].lower().replace(" ",""))][int(QUIZ)-1] == 0: #Newer entries come first in the list, so a duplicate entry is skipped.
+            for i in range(2,amount_of_questions):
+                if pick[i] == comparison[i-1]: 
+                    weekly_score = weekly_score + 1
+            scoreboard[(pick[0].lower().replace(" ",""),pick[1].lower().replace(" ",""))][int(QUIZ)-1] = weekly_score
+    except:
+        with open('non_registered.txt', 'a') as a: #The entrant is not in the registrants. Score not updated.
+            a.write(str((pick[0],pick[1])) + " in Week " + str(QUIZ) + "\n")
+
+#This loop is for week CSVs which are not contained in a selections.csv or registrants.csv file.
+inde = 0
+for week in selections_list:
+    QUIZ = selections_list_num[inde]
+    #We zero out the arrays if this week's score in the scoreboard in case it was overwritten with an incomplete file earlier.
+    for the_score in scoreboard.values():
+        the_score[QUIZ] = 0
+    for answer in answers:
+        #This finds the week of the given pick
         try:
-            scoreboard[(pick[0],pick[1])][int(QUIZ)-1] = quiz_score
+            week_lookat = int(answer[0])
         except:
-            with open('non_registered.txt', 'a') as a: #The entrant is not in the registrants. Score not updated.
-                a.write(str((pick[0],pick[1])) + " in Week " + str(QUIZ))
+            week_lookat = 0 #Week is not an integer in this column so it is invalid and ust be skipped.
+        if int(week_lookat) == QUIZ:
+            comparison = answer.copy()
+            amount_of_questions = len(comparison)
+            break
+    for pick in week:
+        weekly_score = 0
+        try:
+            if scoreboard[(pick[0].lower().replace(" ",""),pick[1].lower().replace(" ",""))][int(QUIZ)-1] == 0: #Newer entries come first in the list, so a duplicate entry is skipped.
+                for i in range(1,(amount_of_questions+1)):
+                    if pick[i].lower() == comparison[i-1].lower():
+                        weekly_score = weekly_score + 1
+                    scoreboard[(pick[0].lower().replace(" ",""),pick[1].lower().replace(" ",""))][int(QUIZ)-1] = weekly_score
+        except:
+            with open('non_registered.txt', 'a') as a: #The entrant is not in the entrants' list. Score not updated.
+                a.write(str((pick[0],pick[1])) + " in Week " + str(QUIZ) + "\n")
+    #This space seems to cause the following two statements not to execute if it isn't here.
+    inde = inde + 1 #Looks at the next entry in selections_list_num.
+    comparison = list() #resetting
 
-#This loop is for quiz CSVs which are not contained in a picks.csv or registrants.csv file.
-for quiz in student_answers_file:
-    ind = 0
-    QUIZ = picks_list_num[ind]
-    for answer in answer_key:
-        #This finds the quiz of the given pick
-        if answer[0] == QUIZ:
-            comparison = answer[:]
-            amount_of_quizzes = len(comparison) - 4
-    for pick in quiz:
-        quiz_score = 0
-        if scoreboard[(pick[0],pick[1])][int(QUIZ)-1] == 0: #Newer entries come first in the list, so a duplicate entry is skipped.
-            for i in range(2,amount_of_quizzes):
-                if pick[i] == comparison[i-1]:
-                    quiz_score = quiz_score + 1
-            try:
-                scoreboard[(pick[0],pick[1])][int(QUIZ)-1] = quiz_score
-            except:
-                with open('non_registered.txt', 'a') as a: #The entrant is not in the registrants. Score not updated.
-                    a.write(str((pick[0],pick[1])) + " in Week " + str(QUIZ))
-    ind = i + 1 #Looks at the next entry in picks_list_num.
-
-#Given the quizly scores, we now compute all scores.
+#Given the weekly scores, we now compute all scores.
 for score in scoreboard:
     total = 0
-    for i in range(0,25):
+    for i in range(0,TOTAL_QUIZZES):
         total = total + scoreboard[score][i]
-    scoreboard[score][25] = total
-    with open("out.csv", "a") as f:
-        output = str(score[0]) + "," + str(score[1]) + "," + str(scoreboard[score][-1])
-        f.write(output)
+    scoreboard[score][TOTAL_QUIZZES] = total
+    with open("out.csv", "a", encoding="utf8") as f:
+        output_string = str(score[0]) + "," + str(score[1]) + ","
+        for k in range (0,(TOTAL_QUIZZES+1)): #The total is TOTAL_QUIZZES+1 because we write the final score too.
+            output_string = output_string + str(scoreboard[score][k]) + ","
+        output_string = output_string + "\n"
+        f.write(output_string)
